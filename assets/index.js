7093f2091eb5181cc59d62649809abd642a7b103
grumpybuffalo <grumpybuffalo@gmail.com>
2019-02-24 14:01:27 -0600

    Nav, wedding party, other small changes

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+function showNav() {
+  document.body.style.overflow = "hidden";
+  document.querySelector("nav").style.height="100%";
+}
+
+function hideNav() {
+  document.body.style.overflow = "";
+  document.querySelector("nav").style.height="";
+}
