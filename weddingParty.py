0f2d87ede61ed3fc7cb0058e02de1afadcc54b54
williamhoza <grumpybuffalo@gmail.com>
2019-06-04 21:12:12 -0500

    Add wedding party descriptions

--- weddingParty.py
+++ weddingParty.py
@@ -18,6 +18,9 @@ def compile():
           <div class="party-title">
             MAID OF HONOR
           </div>
+          <div class="party-description">
+            Alicia's best friend from high school. Rest assured they'll entertain and distract each other during the reception with tic-tac-toe -- just like they did in their high school theology class.
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="nicholas.jpg" />
@@ -26,6 +29,9 @@ def compile():
           </div>
           <div class="party-title">
             BEST MAN
+          </div>
+          <div class="party-description">
+            William's younger older brother. They shared a room growing up. Some nights Mom would make Nicholas sleep on the sofa so they would stop talking. Other nights they would compete to fall asleep first. The rule was, anyone who remembered the competition had to say so and check if the other was awake.
           </div>
         </div>
       </div>
@@ -39,6 +45,9 @@ def compile():
           <div class="party-title">
             BRIDESMAID
           </div>
+          <div class="party-description">
+            Alicia's roommate, guinea pig sitter, and the couple's best third wheel. She lives in daily fear that William and Alicia will name their children after cheeses. (C'mon, Brie is a name!)
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="jimmy.jpg" />
@@ -47,6 +56,9 @@ def compile():
           </div>
           <div class="party-title">
             GROOMSMAN
+          </div>
+          <div class="party-description">
+            William's older older brother. As the first child in the family, he gave all the other kids a model to look up to and emulate, e.g., when he left a snake on the kitchen counter and forgot about it.
           </div>
         </div>
       </div>
@@ -60,6 +72,9 @@ def compile():
           <div class="party-title">
             BRIDESMAID
           </div>
+          <div class="party-description">
+            She married into the Hoza family and survived, so Alicia is (pretty) sure she can do it too. As William's brother's wife, Emily and Alicia are going to be sisters-in-law and no one can tell them differently!
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="matt.jpg" />
@@ -68,6 +83,9 @@ def compile():
           </div>
           <div class="party-title">
             GROOMSMAN
+          </div>
+          <div class="party-description">
+            William's friend from college. Ask him an impossible and ridiculous question. He'll probably enjoy the taste of his own medicine.
           </div>
         </div>
       </div>
@@ -81,6 +99,9 @@ def compile():
           <div class="party-title">
             BRIDESMAID
           </div>
+          <div class="party-description">
+            One of Alicia's close friends, she keeps Alicia up-to-date with internet slang and provides much needed love and acceptance when Alicia needs it.
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="simon.jpg" />
@@ -89,6 +110,9 @@ def compile():
           </div>
           <div class="party-title">
             GROOMSMAN
+          </div>
+          <div class="party-description">
+            William's friend from high school. He lives in Maryland, but he wanted to surprise William for his birthday last year, so he convinced Gehn (coming up next) to come along and coordinated with Alicia who threw an amazing surprise party. William never saw any of it coming.
           </div>
         </div>
       </div>
@@ -102,6 +126,9 @@ def compile():
           <div class="party-title">
             BRIDESMAID
           </div>
+          <div class="party-description">
+            She and Alicia clicked from the day they met. She needs to be by Alicia's side throughout the wedding day to laugh at Alicia's jokes.
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="gehn.jpg" />
@@ -110,6 +137,9 @@ def compile():
           </div>
           <div class="party-title">
             GROOMSMAN
+          </div>
+          <div class="party-description">
+            William's friend from high school. One time he and William drove to Portland to get a tour of a factory that made pipes and things. It was pretty cool.
           </div>
         </div>
       </div>
@@ -123,6 +153,9 @@ def compile():
           <div class="party-title">
             MOTHER OF THE BRIDE
           </div>
+          <div class="party-description">
+            Easily the most fun mom of any Alicia has ever met. Alicia is only mildly concerned that she's becoming more like her with each passing year.
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="roseanne.jpg" />
@@ -131,6 +164,9 @@ def compile():
           </div>
           <div class="party-title">
             MOTHER OF THE GROOM
+          </div>
+          <div class="party-description">
+            She has made William truly vast quantities of customized, delicious, gluten-free food over the years. Sometimes William feels bad and tries to tell her to not go to the trouble, but she's unstoppable.
           </div>
         </div>
       </div>
@@ -144,6 +180,9 @@ def compile():
           <div class="party-title">
             FATHER OF THE BRIDE
           </div>
+          <div class="party-description">
+            William's best audience for punny jokes. He's made sure Alicia has never had to worry about moving, changing the oil in her car, or being unprepared for the future.
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="bradley.jpg" />
@@ -152,6 +191,9 @@ def compile():
           </div>
           <div class="party-title">
             FATHER OF THE GROOM
+          </div>
+          <div class="party-description">
+            He taught William how to skillfully craft premium jokes.
           </div>
         </div>
       </div>
@@ -165,6 +207,9 @@ def compile():
           <div class="party-title">
             FLOWER GIRL
           </div>
+          <div class="party-description">
+            Alicia's little sister (or is she her clone?) wants to know "can we get some action at this wedding? Maybe a swimming pool?"
+          </div>
         </div>
         <div class="party-member">
           <img class="party-photo" src="katrina.jpg" />
@@ -173,6 +218,9 @@ def compile():
           </div>
           <div class="party-title">
             FLOWER GIRL
+          </div>
+          <div class="party-description">
+            William's niece. Here's a poem she wrote: "I have carrots / I have peas / I have all the western seas." &copy; 2018, all rights reserved
           </div>
         </div>
       </div>
@@ -185,6 +233,9 @@ def compile():
           </div>
           <div class="party-title">
             RING BEARER
+          </div>
+          <div class="party-description">
+            William's nephew. He once judged a bird-noise-making competition between William and Alicia.
           </div>
         </div>
       </div>
